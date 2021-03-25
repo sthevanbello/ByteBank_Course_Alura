aeacf4564b6b3eb070a7603372fe5927c3ff5338
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-25 11:44:21 -0300

    Course Alura POO Basic

---
+++ ByteBank_Examples_Alura_POO/Entities/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_Examples_Alura_POO
+{
+    class Account
+    {
+        public string Holder { get; set; }
+        public int Agency { get; set; }
+        public int Number { get; set; }
+
+        public double Balance = 100;
+
+        public bool Withdraw(double value)
+        {
+            if (Balance < value)
+            {
+                return false;
+            }
+            Balance -= value;
+            return true;
+        }
+
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public bool Transfer(double value, Account accDesnity)
+        {
+            if (Balance < value)
+            {
+                return false;
+            }
+
+            Balance -= value;
+            accDesnity.Deposit(value);
+            return true;
+        }
+    }
+}
---
+++ ByteBank_Examples_Alura_POO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ByteBank_Examples_Alura_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account acc1 = new Account() { Holder = "Léo" };
+            acc1.Balance += 150;
+            Console.WriteLine($"Balance: {acc1.Balance:C}");
+
+            bool withdraw = acc1.Withdraw(250);
+            
+            if (withdraw)
+            {
+            Console.WriteLine($"Balance after withdraw: {acc1.Balance:C}");
+            }
+
+            acc1.Deposit(500);
+
+            Console.WriteLine($"Balance after deposit: {acc1.Balance:C}");
+
+            Account acc2 = new Account() {Holder = "Gabriel" };
+
+            acc1.Transfer(350, acc2);
+            Console.WriteLine($"\nDeposit of the $350.00 of the {acc1.Holder} to {acc2.Holder}");
+            Console.WriteLine($"\nBalance Account to: {acc1.Holder} = {acc1.Balance:C}");
+            Console.WriteLine($"Balance Account to: {acc2.Holder} = {acc2.Balance:C}");
+
+            Console.ReadKey();
+        }
+            
+    }
+}
